ed68bb625b32bd4fbcee31d07f505039abf503bd
Adrian <adrian@codeforaustralia.org>
2020-07-16 15:29:47 +1000

    Landing page image updated

---
+++ src/components/common/Section/Section.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Section = (props) => {
+  return (
+    <div className="dic-la">
+      <div className="dic-la-wrapper">
+        <div className="dic-la-w-l1">
+          <div className="dic-la-w-l11">
+            <h1 className="dic-la-w-l11-h1">{props.title}</h1>
+          </div>
+          {props.children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Section;
--- src/containers/Dictionary/Dictionary.js
+++ src/containers/Dictionary/Dictionary.js
@@ -2,21 +2,17 @@ import React from 'react';
 import Search from '../../components/Dictionary/Search/Search';
 import Notification from '../../UI/Notification/Notification';
 import DictionaryState from '../../context/dictionary/DictionaryState';
+import Section from '../../components/common/Section/Section';
+import BackButton from '../../components/BackButton/BackButton';
 
 const Dictionary = () => {
   return (
     <DictionaryState>
       <Notification />
-      <div className="dic-la">
-        <div className="dic-la-wrapper">
-          <div className="dic-la-w-l1">
-            <div className="dic-la-w-l11">
-              <h1 className="dic-la-w-l11-h1">Legal dictionary</h1>
-            </div>
-            <Search />
-          </div>
-        </div>
-      </div>
+      <Section title="Legal dictionary">
+        <Search />
+        <BackButton />
+      </Section>
     </DictionaryState>
   );
 };
