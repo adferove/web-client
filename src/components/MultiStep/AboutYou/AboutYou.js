a6ff9592aaad7579dc06bd6361c2d1a4857b69e4
Adrian <adrian@codeforaustralia.org>
2020-07-20 13:41:40 +1000

    About you initial commit

--- src/components/MultiStep/AboutYou/AboutYou.js
+++ src/components/MultiStep/AboutYou/AboutYou.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
+import TriageContext from '../../../context/triage/triageContext';
+import Section from '../../common/Section/Section';
 
 const AboutYou = () => {
-  return <div></div>;
+  const triageContext = useContext(TriageContext);
+  const { selectedOption } = triageContext;
+  let style = [
+    'land-la-problem-img',
+    'land-la-problem-img__' + selectedOption.image,
+  ].join(' ');
+  return (
+    <Fragment>
+      <Section preTitle="Your legal problem is" title={selectedOption.title}>
+        <div className={style} />
+        <div className="land-la-w-l11">
+          <div className="land-la-w-l112-p">{selectedOption.desc}</div>
+        </div>
+      </Section>
+    </Fragment>
+  );
 };
 
 export default AboutYou;
