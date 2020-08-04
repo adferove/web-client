0f6c12be0c9507256922219c3bc9746c807283ae
Adrian <adferove@hotmail.com>
2020-08-05 00:55:42 +1000

    Landing page updated

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Section from '../common/Section/Section';
+import pages from '../../common/data/pages.json';
 import { Link } from 'react-router-dom';
 
 const Landing = () => {
@@ -8,65 +10,50 @@ const Landing = () => {
       behavior: 'smooth',
     });
   };
+  const page = pages.find((item) => item.page === 'landing');
+
   return (
-    <div className="land-la">
-      <div className="land-la-wrapper">
-        <div className="land-la-w-l1">
-          <div className="land-la-w-l11">
-            <h1 className="land-la-w-l11-h1">Your questions answered</h1>
+    <Fragment>
+      <Section preTitle={page.preTitle} title={page.title}>
+        <div className="land-la-w-img" />
+      </Section>
+      <Section grey>
+        <div className="dic-la-w-l11 dic-la-w-l11__no-bold">
+          {page.paragraphs.map((item) => {
+            let itemStyle = `dic-yg-2 dic-yg-2-img dic-yg-2-img__${item.icon}`;
+            return (
+              <div
+                key={item.id}
+                className={itemStyle}
+                dangerouslySetInnerHTML={{ __html: item.paragraph }}
+              ></div>
+            );
+          })}
+        </div>
+      </Section>
+      <Section grey>
+        <div className="dic-la-w-l11">
+          <div>
+            <Link
+              className="triage-button triage-button--highlight"
+              to="/tool"
+              onClick={scrollToTop}
+            >
+              Start
+            </Link>
+          </div>
+          <div>
+            <Link
+              className="triage-button triage-button--outline"
+              to="/contact-legal-aid"
+              onClick={scrollToTop}
+            >
+              I don't feel safe
+            </Link>
           </div>
         </div>
-        <img
-          alt=""
-          className="land-la-w-img"
-          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII="
-        />
-        <div className="land-la-w-l1">
-          <div className="land-la-w-l11">
-            <div className="land-la-w-l111">
-              <h2 className="land-la-w-l111-h2">About this self-help tool</h2>
-            </div>
-            <div className="land-la-w-l112">
-              <p className="land-la-w-l112-p">
-                Begin your legal journey with this 5-minute tool to understand
-                your problem and see what resources are available for your case.
-                You control how much information you provide, which will remain
-                anonymous through the tool.
-              </p>
-            </div>
-            <div className="land-la-w-l111">
-              <h2 className="land-la-w-l111-h2">What happens next</h2>
-            </div>
-            <div className="land-la-w-l112">
-              <p className="land-la-w-l112-p">
-                We'll ask you a few questions and match you with any of our
-                services that may be able to assist.
-              </p>
-            </div>
-          </div>
-          <div className="land-la-w-l11">
-            <div>
-              <Link
-                onClick={scrollToTop}
-                className="triage-button triage-button--highlight"
-                to="/tool"
-              >
-                Start
-              </Link>
-            </div>
-            <div>
-              <Link
-                className="triage-button triage-button--outline"
-                to="/contact-legal-aid"
-              >
-                I don't feel safe
-              </Link>
-            </div>
-          </div>
-          <div className="land-la-w-l12"></div>
-        </div>
-      </div>
-    </div>
+      </Section>
+    </Fragment>
   );
 };
 
