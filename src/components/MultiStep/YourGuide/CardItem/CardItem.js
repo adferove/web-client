a90c7dc9f8429e8be8511b3a8e3328fea5e97e51
Adrian <adferove@hotmail.com>
2020-08-13 09:51:50 +1000

    Summary page updated

--- src/components/MultiStep/YourGuide/CardItem/CardItem.js
+++ src/components/MultiStep/YourGuide/CardItem/CardItem.js
@@ -5,18 +5,25 @@ const CardItem = (props) => {
   return (
     <div className="nsw-card">
       <div className="nsw-card__content-yg">
-        <div className="dic-def-close">
-          <i id="btn-quick-exit" className="material-icons">
-            open_in_new
-          </i>
-        </div>
-        <p>
-          <strong>{title}</strong>
-        </p>
-        <div
-          className="dic-def__no-bold"
-          dangerouslySetInnerHTML={{ __html: text }}
-        ></div>
+        <a
+          className="nsw-card__card-link"
+          href={link}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <div className="dic-def-close">
+            <i id="btn-quick-exit" className="material-icons">
+              open_in_new
+            </i>
+          </div>
+          <p>
+            <strong>{title}</strong>
+          </p>
+          <div
+            className="dic-def__no-bold"
+            dangerouslySetInnerHTML={{ __html: text }}
+          ></div>
+        </a>
       </div>
     </div>
   );
